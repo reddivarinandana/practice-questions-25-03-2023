be57e57db91264247a9c6090d19877187081de47
reddivarinandana <nadanareddy1122@gmail.com>
2023-03-25 20:47:09 +0530

    integer-to-binary

--- integer-to-binary.java
+++ integer-to-binary.java
@@ -3,6 +3,6 @@
 public class Main{
     public static void main(String[] args){
         int a=92;
-        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(a)) ;
     }
 }
